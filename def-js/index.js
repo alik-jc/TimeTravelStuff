24bd8cd97fee0b4cc153beae3907e9348819fd77
alik-jc <jesusdaniel723@gmail.com>
2025-11-05 11:56:23 -0500

    Agregar soporte para Socket.IO y recarga en vivo

--- def-js/index.js
+++ def-js/index.js
@@ -38,3 +38,4 @@ const numRand = (minimo, maximo) => {
     return random;
 };
 */
+document.getElementById("element").innerHTML = "E!";
--- def-js/server.js
+++ def-js/server.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const app = express();
 const puerto = 3000;
+
+// Crear servidor HTTP y conectar socket.io
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname)));
+
+// Ruta principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.listen(puerto, () => {
+
+// Escuchar conexiones de socket.io
+io.on('connection', (socket) => {
+    console.log('Cliente conectado');
+});
+
+// Iniciar el servidor
+server.listen(puerto, () => {
     console.log(`Servidor corriendo en http://localhost:${puerto}`);
+});
+
+// Emitir un evento de recarga cuando el servidor se reinicie
+process.once('SIGUSR2', () => {
+    io.emit('recargar'); // Emitir el evento a todos los clientes conectados
+    process.kill(process.pid, 'SIGUSR2'); // Permitir que nodemon reinicie el servidor
 });
